4566ef5b84c93af2b1dea3f321c60f7404a555f4
Rayaa Ghirri <rghirri@gmail.com>
2022-10-02 15:19:51 +0100

    completed debounce method

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ gallery.forEach( elementData => {
 
 // console.log(window.innerHeight);
 
-const scrollAmount = -600;
-window.addEventListener('scroll', (e) => {
+// const scrollAmount = -395;
+// window.addEventListener('scroll', (e) => {
     // console.log(e)
-    const { top } = heroSection.getBoundingClientRect();
-    const heroInView = top - window.innerHeight < scrollAmount;
+    // const { top } = heroSection.getBoundingClientRect();
+    // const heroInView = top - window.innerHeight < scrollAmount;
 
-    imageOne.style.opacity = +!heroInView;
-    imageTwo.style.opacity = +heroInView;
+    // imageOne.style.opacity = +!heroInView;
+    // imageTwo.style.opacity = +heroInView;
     
     // imageOne.style.opacity = heroInView ? 0 : 1;
     // imageTwo.style.opacity = heroInView ? 1 : 0;
@@ -43,6 +43,63 @@ window.addEventListener('scroll', (e) => {
     //     //     heroSection.style.backgroundImage = `url(${element.imageOne})`;
     //     // });
     // }
-});
+// });
+
+function debounce(func, wait, immediate) {
+    var timeout;
+  
+    return function executedFunction() {
+      var context = this;
+      var args = arguments;
+          
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+  
+      var callNow = immediate && !timeout;
+      
+      clearTimeout(timeout);
+  
+      timeout = setTimeout(later, wait);
+      
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+function galleryScroll(e){
+    console.count(e);
+    const { top } = heroSection.getBoundingClientRect();
+    const heroInView = top - window.innerHeight < scrollAmount;
+    imageOne.style.opacity = +!heroInView;
+    imageTwo.style.opacity = +heroInView;
+}
+
+const scrollAmount = -395;
+window.addEventListener('scroll', debounce(galleryScroll, 30, true));
+
+
+    // console.log(e)
+    
+    
+    // imageOne.style.opacity = heroInView ? 0 : 1;
+    // imageTwo.style.opacity = heroInView ? 1 : 0;
+
+    // if ( top - window.innerHeight < scrollAmount ) {
+    //     imageOne.style.opacity = 0;
+    //     imageTwo.style.opacity = 1;
+    //     // gallery.forEach( element => {
+    //     //     // console.log(heroSection.getBoundingClientRect());
+    //     //     heroSection.style.backgroundImage = `url(${element.imageTwo})`;
+    //     // });
+    // } else {
+    //     imageOne.style.opacity = 1;
+    //     imageTwo.style.opacity = 0;
+    //     // gallery.forEach( element => {
+    //     //     // console.log(heroSection.getBoundingClientRect());
+    //     //     heroSection.style.backgroundImage = `url(${element.imageOne})`;
+    //     // });
+    // }
+// });
 
 
